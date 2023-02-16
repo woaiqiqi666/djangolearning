5206e4205070b6ac079ca99eb5546304cd7074a0
woaiqiqi666 <forwilddreams@163.com>
2023-02-16 21:23:07 +0800

    'restart'

--- talkfree/talkfree/asgi.py
+++ talkfree/talkfree/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for learning project.
+ASGI config for talkfree project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkfree.settings')
 
 application = get_asgi_application()
--- talkfree/talkfree/wsgi.py
+++ talkfree/talkfree/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for learning project.
+WSGI config for talkfree project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkfree.settings')
 
 application = get_wsgi_application()
